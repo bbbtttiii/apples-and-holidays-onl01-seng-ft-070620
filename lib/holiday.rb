dbe86298d43ee6606ec729fadf54edcc2b8fc36a
Ben Iburg <ben.iburg@gmail.com>
2020-07-09 23:59:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holidays, supplies|
-      if supplies.include?("BBQ")
+      holiday if supplies.include?("BBQ")
     end
   end
 end
